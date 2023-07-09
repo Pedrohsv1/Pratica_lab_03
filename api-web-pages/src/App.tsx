661f74476ed6e65556fa28489634976db663bf0e
Pedro Varela <pedrao.varela@gmail.com>
2023-07-09 19:27:26 -0300

    feat: css and filter products

--- api-web-pages/src/App.tsx
+++ api-web-pages/src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import Produtos from "./pages/produtos";
 
 function App() {
--- api-web-pages/src/pages/produtos.tsx
+++ api-web-pages/src/pages/produtos.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import api from "../services/api";
+import './produtos.css'
 
 interface IProdutos {
   brand: string;
@@ -37,60 +38,93 @@ title: "iPhone 9"
 
 function Produtos() {
   const [produtos, setProdutos] = useState<IProdutos[]>([]);
+  const [filteredList, setFilteredList] = useState<IProdutos[]>([])
+  const [selectedFilter, setSelectedFilter] = useState<string>('')
+
+
+  const filter = [
+    'smartphones',
+    'laptops'
+  ]
 
   useEffect(() => {
     const carregarProdutos = async () => {
-      await api
-        .get("/products?limit=10")
-        .then((response: any) => response.data.products)
-        .then((prod: IProdutos[]) => setProdutos(prod));
+      const response = await api.get("/products?limit=10");
+      const prod: IProdutos[] = response.data.products;
+      setProdutos(prod);
+      setFilteredList(prod);
     };
 
     void carregarProdutos();
   }, []);
 
+  const setFilter = (filtro: string) => {
+    if (selectedFilter === filtro) {
+      setSelectedFilter('');
+      setFilteredList(produtos);
+    } else {
+      setSelectedFilter(filtro);
+      setFilteredList(produtos.filter(prod => prod.category === filtro));
+    }
+  };
+  
   return (
-    <div style={{ width: "100vw", display: "flex", flexDirection: "column" }}>
-      <p>Produtos</p>
-      {produtos.map((prod: IProdutos) => {
-        return (
-          <div key={prod.id}>
-            <h2>{prod.title}</h2>
-            <div
-              style={{
-                margin: "auto",
-                height: "50vh",
-                width: "50vw",
-                backgroundColor: "white",
-              }}
-            >
-              <img
-                src={prod.images[0]}
-                alt=""
+    <div className="wrapper">
+      <h1 style={{padding: '2em'}}>Produtos</h1>
+      <div className="filter">
+        {filter.map((filtro: string, index) => (
+          <button className="filterUnic" key={index} style={{backgroundColor: filtro === selectedFilter ? 'rgba(255, 255, 255, 0.08)' : '#121212'}} onClick={() => {setFilter(filtro)}}>
+            <p>{filtro}</p>
+          </button>
+        ))
+        }
+        
+      </div>
+      <div style={{flexWrap: 'wrap', display: "flex", alignItems: 'center', justifyContent: 'center', gap: '2em', padding: '2em'}}>
+        {filteredList.map((prod: IProdutos) => {
+          return (
+            <div key={prod.id} className="card" style={{ width: "200px", display: "flex", flexDirection: "column", alignItems: 'center', justifyContent: 'center', borderRadius: '4px', overflow:'hidden'  }}>
+
+              <div
                 style={{
-                  height: "50vh",
-                  width: "50vw",
-                  objectFit: "contain",
+                  height: "100px",
+                  width: "200px",
+                  backgroundColor: "#fff",
+                  position: 'relative',
+
                 }}
-              />
-            </div>
-            <div>
-              <p>{prod.price},00</p>
-              <div>
-                <p>{Math.trunc(prod.discountPercentage)}% Sale</p>
-                <p>
-                  {Math.trunc(
-                    prod.price - prod.price * (prod.discountPercentage / 100)
-                  )}
-                  ,00
-                </p>
+              >
+                <img
+                  src={prod.thumbnail}
+                  alt=""
+                  style={{
+                    height: "100px",
+                    width: "200px",
+                    objectFit: "contain",
+                  }}
+                />
+                <div style={{width: '80px', position: 'absolute', translate: '-50% 0%', bottom: 10, left:'50%', backgroundColor: "#121212", padding: '0.25em 0.5em', border: '1px solid rgba(255, 255, 255, 0.08)', borderRadius: '20px',display: "flex", flexDirection: "column", alignItems: 'center', justifyContent: 'center'}}>
+                  <p>{prod.price},00</p>
+                </div>
+              </div>
+              <div style={{flexDirection: 'column', display: "flex", height: '10em', padding: '1em', alignItems: 'center', gap: '1em', marginTop: '1em' }}>
+              <p><strong>{prod.title}</strong></p>   
+              <p style={{color: 'rgba(255, 255, 255, 0.7)'}}>{prod.category}</p>
+                  
+                  {/* <p>{Math.trunc(prod.discountPercentage)}% Sale</p>
+                  <p>
+                    {Math.trunc(
+                      prod.price - prod.price * (prod.discountPercentage / 100)
+                    )}
+                    ,00
+                  </p>*/}
+
               </div>
             </div>
-
-            <p>{prod.description}</p>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
+      
     </div>
   );
 }
